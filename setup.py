df1a34e01299fd1f7caa615ab069eb1b83a423e1
liminspace <igormeln@gmail.com>
2017-05-02 17:02:23 +0300

    Add django 1.11 and python 3.6 support.

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
     description='Django Private URL',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     license='MIT',
-    author='Igor Melnyk',
+    author='Igor Melnyk @liminspace',
     author_email='liminspace@gmail.com',
     url='https://github.com/liminspace/django-privateurl',
     packages=find_packages(exclude=('tests', 'tests.*')),
     include_package_data=True,
     zip_safe=False,  # because include static
     install_requires=[
-        'django>=1.8,<1.11',
+        'django>=1.8,<1.12',
     ],
     keywords=[
         'django', 'url', 'private', 'private url', 'django-privateurl',
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
